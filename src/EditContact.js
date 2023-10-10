879f06d749029f1a8117536ffbb94c12bfd96437
MatthewLampard <mlampar@uwo.ca>
2023-10-09 21:10:15 -0400

    Fixed edit contact bug

--- src/EditContact.js
+++ src/EditContact.js
@@ -23,48 +23,64 @@ function EditContact() {
   }, [index]);
 
   const onSubmit = (data) => {
-    // Update the contact with the edited data
-    const updatedContact = { ...contact, ...data };
-
     // Load contacts from localStorage
     const storedContacts = localStorage.getItem(LOCAL_STORAGE_KEY);
     const parsedContacts = storedContacts ? JSON.parse(storedContacts) : [];
-
+  
+    // Retrieve the original contact
+    const originalContact = parsedContacts[index] || {};
+  
+    // Initialize an object to store the updated contact data
+    const updatedContact = {};
+  
+    // Compare each field with the original contact data
+    for (const key in originalContact) {
+      if (originalContact.hasOwnProperty(key)) {
+        updatedContact[key] = data[key] || originalContact[key];
+      }
+    }
+  
     // Update the contact in the parsedContacts array
     parsedContacts[index] = updatedContact;
-
+  
     // Save the updated contacts array back to localStorage
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(parsedContacts));
-
+  
     // Redirect or navigate back to the main contacts page
     navigate('/'); // Redirect to the main contacts page
   };
+  
 
   return (
     <div>
       <h2>Edit Contact</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
+          name='firstName'
           placeholder='First Name'
           {...register('firstName')}
           defaultValue={contact.firstName}
         />
         <input
+          name='lastName'
           placeholder='Last Name'
           {...register('lastName')}
           defaultValue={contact.lastName}
         />
         <input
+          name='phone'
           placeholder='Phone'
           {...register('phone')}
           defaultValue={contact.phone}
         />
         <input
+          name='email'
           placeholder='Email'
           {...register('email')}
           defaultValue={contact.email}
         />
         <input
+          name='address'
           placeholder='Address'
           {...register('address')}
           defaultValue={contact.address}
