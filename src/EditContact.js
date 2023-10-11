30fb9a11a5d2eecc56d41aa727f47aaf0cb1f3aa
MatthewLampard <mlampar@uwo.ca>
2023-10-11 12:35:57 -0400

    Styled the edit contact page

--- src/EditContact.js
+++ src/EditContact.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate, useParams } from 'react-router-dom';
+import './styles.css';
 
 const LOCAL_STORAGE_KEY = 'contactListApp.contacts';
 
@@ -53,42 +54,49 @@ function EditContact() {
   
 
   return (
-    <div>
-      <h2>Edit Contact</h2>
+    <>
+    <div className="container">
       <form onSubmit={handleSubmit(onSubmit)}>
+      <h2>Edit Contact</h2>
         <input
           name='firstName'
           placeholder='First Name'
           {...register('firstName')}
           defaultValue={contact.firstName}
+          className="form-input" // Apply the input style
         />
         <input
           name='lastName'
           placeholder='Last Name'
           {...register('lastName')}
           defaultValue={contact.lastName}
+          className="form-input" // Apply the input style
         />
         <input
           name='phone'
           placeholder='Phone'
           {...register('phone')}
           defaultValue={contact.phone}
+          className="form-input" // Apply the input style
         />
         <input
           name='email'
           placeholder='Email'
           {...register('email')}
           defaultValue={contact.email}
+          className="form-input" // Apply the input style
         />
         <input
           name='address'
           placeholder='Address'
           {...register('address')}
           defaultValue={contact.address}
+          className="form-input" // Apply the input style
         />
         <button type='submit'>Save Changes</button>
       </form>
     </div>
+    </>
   );
 }
 
